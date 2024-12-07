4d2eb77422af5b4a1703ba6ae62ca4d444168dd7
Melhi95 <melhi.92@mail.ru>
2024-12-07 09:55:26 +0300

    Создание бина Timer и связывание с AnimalsCage

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -11,11 +11,14 @@ public class AnimalsCage {
     @Qualifier("dog")
     private Animal animal;
 
+    @Autowired
+    private Timer timer;
+
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
