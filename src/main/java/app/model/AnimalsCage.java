7e5ea528505e38175e2b49a836ad4729dcb7f2b8
Melhi95 <melhi.92@mail.ru>
2024-12-07 09:59:24 +0300

    Создание геттера getTimer в AnimalsCage для прохождения теста

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -21,4 +21,8 @@ public class AnimalsCage {
         System.out.println(timer.getTime());
         System.out.println("________________________");
     }
+
+    public Timer getTimer() {
+        return timer;
+    }
 }
